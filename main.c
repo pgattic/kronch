da90786f7798334bccd7ed20242bc2602f40dfd5
pgattic <pgattic@gmail.com>
2024-01-27 03:28:33 -0700

    Functioning Emulator!!!
    HOLY COW IT ACTUALLY WORKS

--- main.c
+++ main.c
@@ -12,16 +12,17 @@ by Preston Corless
 #include "raylib.h"
 
 #define SCREEN_SCALE 10
-#define PRGMEM 4096 // 4KB
-#define WORKMEM 4096 // 4KB
+#define MEMORY 4096 // 4KB
 #define STACKMEM 1024 // 4 bytes per value, for a stack of 256
+#define CLOCK_SPEED 500 // Hertz
+#define REFRESH_RATE 60 // Hertz
+#define CYCLES_PER_FRAME (CLOCK_SPEED/REFRESH_RATE)
 
-unsigned char prg[PRGMEM] = {0}; // 4KB of Program Memory
+unsigned char mem[MEMORY] = {0}; // 4KB of Program Memory
 int pc = 0x200; // Program Counter
 
 unsigned char V[16] = {0}; // V0-VF
 
-unsigned char ram[WORKMEM] = {0};
 unsigned short I; // Memory Pointer - Only uses 12 bits
 
 unsigned short stack[256] = {0}; // 16-bit values; also only uses 12 of those bits
@@ -52,7 +53,7 @@ int loadCode(FILE* f, char* dest) {
     ch = fgetc(f);
     dest[rhead + 0x200] = ch;
     rhead++;
-    if (rhead > PRGMEM) {
+    if (rhead > MEMORY) {
       fprintf(stderr, "\nERROR: Out of Program Memory.");
       return -1;
     }
@@ -80,8 +81,9 @@ void initChip8() {
     0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
     0xF0, 0x80, 0xF0, 0x80, 0x80  // F
   };
-  memset(ram, 0, 4096);
-  memcpy(ram, chip8_fontset, 80 * sizeof(char));
+  memset(mem, 0, 4096);
+  memcpy(&mem[0x50], chip8_fontset, 80 * sizeof(char));
+  memcpy(mem, chip8_fontset, 80 * sizeof(char));
   memset(stack, 0, 256);
   memset(V, 0, 16);
   memset(screen, 0, 2048);
@@ -105,28 +107,6 @@ void readKeys() {
   keyboard[0xD] = IsKeyDown(KEY_R);
   keyboard[0xE] = IsKeyDown(KEY_F);
   keyboard[0xF] = IsKeyDown(KEY_V);
-}
-
-void clearScreen() {
-  for (int i = 0; i < 32; i++) {
-    for (int j = 0; j < 64; j++) {
-      screen[i][j] = false;
-    }
-  }
-  drawFlag = true;
-}
-
-void drawSprite(unsigned char x, unsigned char y, unsigned char height) {
-  for (int row = 0; row < height; row++) {
-    for (int pixel = 0; pixel < 8; pixel++) {
-      bool memPX = (ram[I+row] >> pixel) % 2;
-      if (screen[y+row][x+(7-pixel)] && memPX) {
-        V[0xF] = 1;
-      }
-      screen[y+row][x+(7-pixel)] |= memPX;
-    }
-  }
-  drawFlag = true;
 }
 
 void drawScreen() {
@@ -164,190 +144,213 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  int fileSize = loadCode(f, prg);
+  initChip8();
+
+  int fileSize = loadCode(f, mem);
 
   if (fileSize < 0) {
     return 1;
   }
 
-  initChip8();
-
   InitWindow(64 * SCREEN_SCALE, 32 * SCREEN_SCALE, "Kronch");
-  SetTargetFPS(60);
-  while (pc < PRGMEM && !WindowShouldClose()) {
-    unsigned char op1 = prg[pc];
-    unsigned char op2 = prg[pc+1];
-    unsigned short opcode = (op1 << 8) | op2;
-    char X = op1 & 0x0F;
-    char Y = op2 >> 4;
-    pc += 2;
+  SetTargetFPS(REFRESH_RATE);
+  while (pc < MEMORY && !WindowShouldClose()) {
     readKeys();
-    switch (op1 >> 4) {
-      case 0x0: // Various instructions
-        if (op2 == 0xE0) { // clear screen
-          clearScreen();
-        } else if (op2 == 0xEE) { // Return from subroutine (use stack)
-          stackptr--;
-          pc = stack[stackptr];
-        } else {
-          printf("Opcode Error 0XXX, address %x\n", pc-2);
-        }
-        break;
-      case 0x1: // Goto (exclude stack)
-        pc = opcode & 0x0fff;
-        break;
-      case 0x2: // Subroutine (goto with stack)
-        stack[stackptr] = pc;
-        stackptr++;
-        pc = opcode & 0x0fff;
-        break;
-      case 0x3: // Equality conditional
-        if (V[X] == op2) {
-          pc += 2;
-        }
-        break;
-      case 0x4: // Inequality conditional
-        if (V[X] != op2) {
-          pc += 2;
-        }
-        break;
-      case 0x5: // Equality conditional (compare registers)
-        if (V[X] == V[Y]) {
-          pc += 2;
-        }
-        break;
-      case 0x6: // Set register
-        V[X] = op2;
-        break;
-      case 0x7: // Add to register (no carry flag)
-        V[X] += op2;
-        break;
-      case 0x8: // Register Math
-        switch (op2 & 0x0f) {
-          case 0x0: // Transfer value
-            V[X] = V[Y];
-            break;
-          case 0x1: // OR Operation
-            V[X] |= V[Y];
-            break;
-          case 0x2: // AND Operation
-            V[X] &= V[Y];
-            break;
-          case 0x3: // XOR Operation
-            V[X] ^= V[Y];
-            break;
-          case 0x4:{ // Add Operation
-            int i = (int)V[X] + (int)V[Y];
-            //V[0xF] = (i > 0xFF);
-            if (i > 0xff) {
-              V[0xF] = 1;
-            } else {
-              V[0xF] = 0;
-            }
-            V[X] = i & 0xFF;
-            break;
-          }
-          case 0x5: // Subtract Operation
-            V[0xF] = V[X] >= V[Y];
-            V[X] -= V[Y];
-            break;
-          case 0x6: // Right Bit Shift Operation
-            V[0xF] = V[X] & 0x1;
-            V[X] >>= 1;
-            break;
-          case 0x7: // Subtract Operation
-            V[0xF] = V[Y] >= V[X];
-            V[X] = V[Y] - V[X];
-            break;
-          case 0xE: // Left Bit Shift Operation
-            V[0xF] = (V[X] & 0x80) >> 7;
-            V[X] <<= 1;
-            break;
-          default:
-            printf("Opcode Error 8XXX, address %x\n", pc-2);
-            break;
-        }
-        break;
-      case 0x9: // Register inequality
-        if (V[X] != V[Y]) {
-          pc += 2;
-        }
-        break;
-      case 0xA: // Set I (Memory access pointer)
-        I = opcode & 0x0FFF;
-        break;
-      case 0xB: // Jump to predetermined address
-        pc = V[0] + (opcode & 0x0FFF);
-        break;
-      case 0xC:
-        V[X] = rand() & op2;
-        break;
-      case 0xD:
-        V[0xF] = 0;
-        drawSprite(V[X] % 64, V[Y] % 32, op2 & 0x0F);
-        break;
-      case 0xE:
-        if (op2 == 0x9E) {
-          if (keyboard[V[X]]) {
-            pc += 2;
-          }
-        } else if (op2 == 0xA1) {
-          if (!keyboard[V[X]]) {
-            pc += 2;
-          }
-        }
-        break;
-      case 0xF:
-        switch (op2) {
-          case 0x07:
-            V[X] = delayTimer;
-            break;
-          case 0x0A:{
-            bool isKeyPressed = 0;
-            for (int i = 0; i < 16; i++) {
-              if (keyboard[i]) {
-                isKeyPressed |= keyboard[i];
-                V[X] = i;
+    for (int cycles = 0; cycles < 9 && pc < MEMORY; cycles++) {
+      unsigned char op1 = mem[pc];
+      unsigned char op2 = mem[pc+1];
+      unsigned short opcode = (op1 << 8) | op2;
+      char X = op1 & 0x0F;
+      char Y = op2 >> 4;
+      pc += 2;
+      switch (op1 >> 4) {
+        case 0x0: // Various instructions
+          if (op2 == 0xE0) { // clear screen
+            for (int i = 0; i < 32; i++) {
+              for (int j = 0; j < 64; j++) {
+                screen[i][j] = false;
               }
             }
-            if (!isKeyPressed) {
-              pc -= 2;
-            }
-            break;
+            drawFlag = true;
+          } else if (op2 == 0xEE) { // Return from subroutine (use stack)
+            stackptr--;
+            pc = stack[stackptr];
+          } else {
+            printf("Opcode Error 0XXX, address %x\n", pc-2);
           }
-          case 0x15:
-            delayTimer = V[X];
-            break;
-          case 0x18:
-            soundTimer = V[X];
-            break;
-          case 0x1E:
-            I += V[X];
-            break;
-          case 0x29:
-            I = V[X] * 5;
-            break;
-          case 0x33:{
-              ram[I] = (int)(V[X]/100);
-              ram[I+1] = (int)((V[X] % 100)/10);
-              ram[I+2] = (int)(V[X] % 10);
+          break;
+        case 0x1: // Goto (exclude stack)
+          pc = opcode & 0x0fff;
+          break;
+        case 0x2: // Subroutine (goto with stack)
+          stack[stackptr] = pc;
+          stackptr++;
+          pc = opcode & 0x0fff;
+          break;
+        case 0x3: // Equality conditional
+          if (V[X] == op2) {
+            pc += 2;
+          }
+          break;
+        case 0x4: // Inequality conditional
+          if (V[X] != op2) {
+            pc += 2;
+          }
+          break;
+        case 0x5: // Equality conditional (compare registers)
+          if (V[X] == V[Y]) {
+            pc += 2;
+          }
+          break;
+        case 0x6: // Set register
+          V[X] = op2;
+          break;
+        case 0x7: // Add to register (no carry flag)
+          V[X] += op2;
+          break;
+        case 0x8: // Register Math
+          switch (op2 & 0x0f) {
+            case 0x0: // Transfer value
+              V[X] = V[Y];
+              break;
+            case 0x1: // OR Operation
+              V[X] |= V[Y];
+              break;
+            case 0x2: // AND Operation
+              V[X] &= V[Y];
+              break;
+            case 0x3: // XOR Operation
+              V[X] ^= V[Y];
+              break;
+            case 0x4:{ // Add Operation
+              int i = (int)V[X] + (int)V[Y];
+              //V[0xF] = (i > 0xFF);
+              if (i > 0xff) {
+                V[0xF] = 1;
+              } else {
+                V[0xF] = 0;
+              }
+              V[X] = i & 0xFF;
               break;
             }
-          case 0x55:
-            for (char i = 0; i <= (op1 & 0xF); i++) {
-              ram[I+i] = V[i];
+            case 0x5: // Subtract Operation
+              V[0xF] = V[X] >= V[Y];
+              V[X] -= V[Y];
+              break;
+            case 0x6: // Right Bit Shift Operation
+              V[0xF] = V[X] & 0x1;
+              V[X] >>= 1;
+              break;
+            case 0x7: // Subtract Operation
+              V[0xF] = V[Y] >= V[X];
+              V[X] = V[Y] - V[X];
+              break;
+            case 0xE: // Left Bit Shift Operation
+              V[0xF] = (V[X] & 0x80) >> 7;
+              V[X] <<= 1;
+              break;
+            default:
+              printf("Opcode Error 8XXX, address %x\n", pc-2);
+              break;
+          }
+          break;
+        case 0x9: // Register inequality
+          if (V[X] != V[Y]) {
+            pc += 2;
+          }
+          break;
+        case 0xA: // Set I (Memory access pointer)
+          I = opcode & 0x0FFF;
+          break;
+        case 0xB: // Jump to predetermined address
+          pc = V[0] + (opcode & 0x0FFF);
+          break;
+        case 0xC:
+          V[X] = rand() & op2;
+          break;
+        case 0xD:
+          V[0xF] = 0;
+          int coordX = V[X] % 64;
+          int coordY = V[Y] % 32;
+          int height = op2 & 0xF;
+
+          for (int row = 0; row < height; row++) {
+            unsigned char sprRow = mem[I + row];
+            for (int pixel = 0; pixel < 8; pixel++) {
+              bool filled = (sprRow >> (7-pixel)) & 1;
+              if (filled && screen[coordY + row][coordX + pixel]) {
+                screen[coordY + row][coordX + pixel] = false;
+                V[0xF] = true;
+              } else if (filled) {
+                screen[coordY + row][coordX + pixel] = true;
+              }
             }
-            break;
-          case 0x65:
-            for (char i = 0; i <= (op1 & 0xF); i++) {
-              V[i] = ram[I+i];
+          }
+          drawFlag = true;
+          break;
+        case 0xE:
+          if (op2 == 0x9E) {
+            if (keyboard[V[X]]) {
+              pc += 2;
             }
-            break;
-        }
-        break;
-      default:
-        printf("Opcode Error: %x\n", opcode);
-        break;
+          } else if (op2 == 0xA1) {
+            if (!keyboard[V[X]]) {
+              pc += 2;
+            }
+          }
+          break;
+        case 0xF:
+          switch (op2) {
+            case 0x07:
+              V[X] = delayTimer;
+              break;
+            case 0x0A:{
+              bool isKeyPressed = 0;
+              for (int i = 0; i < 16; i++) {
+                if (keyboard[i]) {
+                  isKeyPressed |= keyboard[i];
+                  V[X] = i;
+                }
+              }
+              if (!isKeyPressed) {
+                pc -= 2;
+              }
+              break;
+            }
+            case 0x15:
+              delayTimer = V[X];
+              break;
+            case 0x18:
+              soundTimer = V[X];
+              break;
+            case 0x1E:
+              I += V[X];
+              break;
+            case 0x29:
+              I = V[X] * 5;
+              break;
+            case 0x33:{
+              mem[I] = (int)(V[X]/100);
+              mem[I+1] = (int)((V[X] % 100)/10);
+              mem[I+2] = (int)(V[X] % 10);
+              break;
+            }
+            case 0x55:
+              for (char i = 0; i <= (op1 & 0xF); i++) {
+                mem[I+i] = V[i];
+              }
+              break;
+            case 0x65:
+              for (char i = 0; i <= (op1 & 0xF); i++) {
+                V[i] = mem[I+i];
+              }
+              break;
+          }
+          break;
+        default:
+          printf("Opcode Error: %x\n", opcode);
+          break;
+      }
     }
     drawScreen();
   }
